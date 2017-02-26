bef9d46fa6df7797f7e1cc0ba1af9c1f35351a48
João Miguel <jrsmiguel@outlook.pt>
2017-02-26 17:20:45 +0000

    Adicionado build

---
+++ build/CMakeFiles/TP1-CG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1-CG.dir/main.cpp.o"
+  "TP1-CG.pdb"
+  "TP1-CG"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1-CG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
