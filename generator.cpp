a0996e7c576005c9938af19928427f4e0b52f054
Paulo Guedes <paulojorge.guedes@hotmail.com>
2017-02-28 00:03:45 +0000

    Box feito

--- generator.cpp
+++ generator.cpp
@@ -14,20 +14,34 @@ void plane(float x, float z, char* ficheiro){
 	fclose(op);
 }
 
-void box(float x, float y, float z, int stacks, char* ficheiro){
-	int r,st;
-	float yaux0,yaux1;
+void box(float x, float y, float z, int stacks, int slices, char* ficheiro){
+	int r,st,sl;
+	float yaux0,yaux1,zaux0,zaux1;
 	//altura das stacks
 	float yaux=y/stacks;
+	//largura das slices
+	float zaux=z/slices;
 	FILE *op = fopen(ficheiro, "w+");
-	//face de baixo
-	fprintf(op,"0 0 0\n%f 0 %f\n0 0 %f\n%f 0 %f\n0 0 0\n%f 0 0\n", x, z, z, x, z,x);
-	//face da direita
-	fprintf(op,"%f 0 0\n%f %f 0\n%f 0 %f\n%f 0 %f\n%f %f 0\n%f %f %f\n", x, x, y, x, z,x,z,x,y,x,y,z);
-	//face de cima
-	fprintf(op,"%f %f %f\n%f %f 0\n0 %f 0\n0 %f 0\n0 %f %f\n%f %f %f\n", x,y,z,x,y,y,y,y,z,x,y,z);
-	//face da esquerda
-	fprintf(op,"0 %f 0\n0 0 0\n0 %f %f\n0 0 0\n0 0 %f\n0 %f %f\n", y,y,z,z,y,z);
+	for(sl=1;sl<=slices;sl++){
+		zaux0=z-(sl-1)*zaux;
+		zaux1=z-sl*zaux;
+		//face de baixo
+		fprintf(op,"0 0 %f\n0 0 %f\n%f 0 %f\n%f 0 %f\n%f 0 %f\n0 0 %f\n",zaux0, zaux1,x, zaux1, x, zaux1,x,zaux0,zaux0);
+		//face de cima
+		fprintf(op,"%f %f %f\n%f %f %f\n0 %f %f\n0 %f %f\n0 %f %f\n%f %f %f\n", x,y,zaux0,x,y,zaux1,y,zaux1,y,zaux1,y,zaux0,x,y,zaux0);
+	}
+	for(st=1;st<=stacks;st++){
+		yaux0=y-(st-1)*yaux;
+		yaux1=y-st*yaux;
+		for(sl=1;sl<=slices;sl++){
+			zaux0=z-(sl-1)*zaux;
+			zaux1=z-sl*zaux;
+			//face da direita
+			fprintf(op,"%f %f %f\n%f %f %f\n%f %f %f\n%f %f %f\n%f %f %f\n%f %f %f\n", x,yaux1,zaux1, x, yaux0,zaux1, x,yaux1, zaux0,x,yaux1, zaux0,x,yaux0,zaux1,x,yaux0,zaux0);
+			//face da esquerda
+			fprintf(op,"0 %f %f\n0 %f %f\n0 %f %f\n0 %f %f\n0 %f %f\n0 %f %f\n", yaux0,zaux1,yaux1,zaux1,yaux1,zaux0,yaux1,zaux0,yaux0,zaux0,yaux0,zaux1);
+		}
+	}
 	for(st=1;st<=stacks;st++){
 		yaux0=y-(st-1)*yaux;
 		yaux1=y-st*yaux;
@@ -65,7 +79,7 @@ int main(int argc, char **argv) {
 	}
 	else{
 		if(strcmp(argv[1],"box")==0){
-			box(atof(argv[2]), atof(argv[3]), atof(argv[4]), atoi(argv[5]), argv[6]);
+			box(atof(argv[2]), atof(argv[3]), atof(argv[4]), atoi(argv[5]), atoi(argv[6]), argv[7]);
 		}
 		else {
 			if(strcmp(argv[1],"cone")==0){
